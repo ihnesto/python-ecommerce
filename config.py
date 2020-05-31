5fb0085d18a2a28ca1a12280708870a9f7d6cb8e
Ihor N <js.rcnit@gmail.com>
2020-05-31 08:34:49 +0300

    main page added

--- config.py
+++ config.py
@@ -1,1 +1,4 @@
 DEBUG_CONFIG = True
+SECRET_KEY = 'hfugorhfjfun40r95004urfgmcn92umxgorr4657uytrrevc'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///ecommerce.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
+import config
 
 app = Flask(__name__)
+
+@app.route('/')
+def index() :
+    return render_template('main-page/page-listing-grid.html')
 
 if __name__ == '__main__' :
     app.run(debug=True)
