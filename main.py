e3556ef31fbad88eea5b1822c6b1ae6ee3983778
Ihor <js.rcnit@gmail.com>
2020-06-02 16:24:43 +0300

    register added

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect, flash, url_for, session
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError, FileField
+from flask_wtf.file import FileRequired, FileAllowed
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from datetime import timedelta
+from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+from flask_bcrypt import Bcrypt
 import config
 
 app = Flask(__name__)
@@ -8,6 +16,12 @@ app.config.from_object(config.EcommerceConfig)
 
 db = SQLAlchemy(app)
 
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 class Product(db.Model):
     __tablename__ = 'products'
@@ -20,10 +34,89 @@ class Product(db.Model):
     def __repr__(self):
         return f"Product({self.desc} - {self.price})"
 
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(40), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User({self.username} - {self.email})"
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=6)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    user_img = FileField('User Image File', validators=[ FileRequired(), FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('This username is exists')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('This email is exists')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register() :  
+    if current_user.is_authenticated:
+        return redirect('/')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account was created', 'success')
+        return redirect('/login')
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect('/')
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data, duration = timedelta(days=5))
+            next_page = request.args.get('next')
+            if next_page :
+                return redirect(next_page)
+            else :
+                return redirect('/')     
+        else:
+            flash('Login Unsuccessful. Please check your email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+@login_required
+def logout():
+    # print(session)
+    logout_user()
+    return redirect('/')
+
 @app.route('/')
 def index() :
     products = Product.query.all()
-    return render_template('main-page/page-listing-grid.html', products=products)
+    return render_template('products.html', products=products)
 
 if __name__ == '__main__' :
     app.run(debug=True)
